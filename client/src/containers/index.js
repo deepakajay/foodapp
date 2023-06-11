4232dde018dbcd3a75d59349132d4869babcc6d5
deepak <deepakajay77@gmail.com>
2023-06-11 22:22:24 +0530

    Inital project setup completed, added firebase, tailwindcss, fonts, router

---
+++ client/src/containers/index.js
@@ -0,0 +1,7 @@
+//this file will export all the components together
+//so we don't have to import multiple componets inside out App file making it complex
+//inside our App file will will import derectly and get access to all the components
+//making the code much more easier
+
+export { default as Main } from "./Main";
+export { default as Login } from "./Login";
